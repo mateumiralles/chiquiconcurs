093984fefdd5f074277287da5fb4e4a550cab16c
Mateumiralles <mateumiralles714@gmail.com>
2023-12-19 21:00:34 +0100

    MainPage Layout

--- app/page.tsx
+++ app/page.tsx
@@ -8,9 +8,32 @@ export default function Home() {
           <div className="h-[800px] w-[800px] bg-gradient-radial from-sky-200 via-blue-200 blur-3xl  dark:from-sky-900 dark:via-[#0141ff] dark:opacity-25"></div>
         </div>
       </div>
-      <h1 className="text-8xl text-white font-extrabold">hello</h1>
 
-      <div className=" grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+      <div className="flex flex-col h-full w-full ">
+        <div className="h-2/3 flex flex-col items-center justify-center pt-20">
+          <h2 className="text-8xl text-white font-extrabold">benvinguda al</h2>
+          <h2 className="text-9xl text-white font-extrabold">chiquiconcurs</h2>
+        </div>
+        <div className="h-1/3 flex flex-col items-center justify-start">
+          <a
+            href="/concursPage"
+            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2 className={`mb-3 text-2xl font-semibold`}>
+              Comença{" "}
+              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                -&gt;
+              </span>
+            </h2>
+            <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+              Clicka aqui per a començar a jugar al concurs!
+            </p>
+          </a>
+        </div>
+      </div>
+      {/* <div className=" grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
@@ -78,7 +101,7 @@ export default function Home() {
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
         </a>
-      </div>
+      </div> */}
     </main>
   );
 }
